df9b829929bd831320d0f10ec7cf1a1c0d17ef7e
jtuusula <juha.juhis@gmail.com>
2013-09-07 11:08:48 +0300

    First release of PlusCalculator Finnish Edition.
    Sorry forks, I haven't put enought time to this apps visual side. It
    looks currently horrible, but it does work.

---
+++ Form1.vb
@@ -0,0 +1,89 @@
+﻿Public Class Form1
+    Public Value1 As Double
+    Public oper As Char
+    Private Sub Button7_Click(sender As System.Object, e As System.EventArgs) Handles Button7.Click
+        ShowValue(Button7)
+    End Sub
+
+    Private Sub Button8_Click(sender As System.Object, e As System.EventArgs) Handles Button8.Click
+        ShowValue(Button8)
+    End Sub
+
+    Private Sub Button9_Click(sender As System.Object, e As System.EventArgs) Handles Button9.Click
+        ShowValue(Button9)
+    End Sub
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        ShowValue(Button6)
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        ShowValue(Button5)
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        ShowValue(Button4)
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        ShowValue(Button3)
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        ShowValue(Button2)
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        ShowValue(Button1)
+    End Sub
+
+    Private Sub Button10_Click(sender As System.Object, e As System.EventArgs) Handles Button10.Click
+        ShowValue(Button10)
+    End Sub
+
+    Private Sub Button13_Click(sender As System.Object, e As System.EventArgs) Handles Button13.Click
+        arithmetic(Button13)
+    End Sub
+
+    Private Sub Button14_Click(sender As System.Object, e As System.EventArgs) Handles Button14.Click
+        arithmetic(Button14)
+    End Sub
+
+    Private Sub Button15_Click(sender As System.Object, e As System.EventArgs) Handles Button15.Click
+        arithmetic(Button15)
+    End Sub
+
+    Private Sub Button16_Click(sender As System.Object, e As System.EventArgs) Handles Button16.Click
+        arithmetic(Button16)
+    End Sub
+
+    Private Sub Button11_Click(sender As System.Object, e As System.EventArgs) Handles Button11.Click
+        Calculate()
+    End Sub
+
+    Private Sub Button12_Click(sender As System.Object, e As System.EventArgs) Handles Button12.Click
+        TextBox1.Text = TextBox1.Text & "."
+    End Sub
+
+    Private Sub Button17_Click(sender As System.Object, e As System.EventArgs) Handles Button17.Click
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button19_Click(sender As System.Object, e As System.EventArgs) Handles Button19.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button18_Click(sender As System.Object, e As System.EventArgs) Handles Button18.Click
+        Dim intResponse As DialogResult
+
+        intResponse = MessageBox.Show _
+                        ("TheJuhish © 2012 - Voit jakaa ohjelmaa vapaasti, koska se on ilmainen.", _
+                         "PlusCalculator", _
+                         MessageBoxButtons.OK, _
+                         MessageBoxIcon.Question)
+
+        If intResponse = DialogResult.Yes Then
+            ' end the program
+        End If
+    End Sub
+End Class
