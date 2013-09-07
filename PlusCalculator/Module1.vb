b4c0fdef4ec676d4f2e95fe1ef5f792bbf8624f2
jtuusula <juha.juhis@gmail.com>
2013-09-07 11:20:27 +0300

    Finnish release of PlusCalculator
    Visual Side isn't so good now, but progman does work.

---
+++ PlusCalculator/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+    Sub ShowValue(ByVal Butt As Button)
+        Form1.TextBox1.Text = Val(Form1.TextBox1.Text & Butt.Text)
+    End Sub
+
+
+    Sub arithmetic(ByVal butt As Button)
+        Form1.Value1 = Val(Form1.TextBox1.Text)
+        Form1.oper = butt.Text
+        Form1.TextBox1.Text = " "
+    End Sub
+
+
+    Sub Calculate()
+        Select Case Form1.oper
+            Case "+"
+                Form1.TextBox1.Text = Form1.Value1 + Val(Form1.TextBox1.Text)
+            Case "-"
+                Form1.TextBox1.Text = Form1.Value1 - Val(Form1.TextBox1.Text)
+            Case "x"
+                Form1.TextBox1.Text = Form1.Value1 * Val(Form1.TextBox1.Text)
+            Case "/"
+                Form1.TextBox1.Text = Form1.Value1 / Val(Form1.TextBox1.Text)
+        End Select
+
+    End Sub
+
+End Module
